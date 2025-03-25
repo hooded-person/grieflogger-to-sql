dde5d3a63ebb344d1c781ea624435b4b73bf5b2e
Niels Groen <hooded.main@gmail.com>
2025-03-25 23:02:23 +0100

    Some testing things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use dotenv::dotenv;
+
+fn main() -> std::io::Result<()>{
+    dotenv().ok();
+    let path_to_file: String = env::var("PROGRESS_LOG").unwrap();
+    let mut file = File::open(&path_to_file)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("{}", contents);
+    Ok(())
 }
