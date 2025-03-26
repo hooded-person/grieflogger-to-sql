e536860e16758f78cae6684884a13ce406925a55
Niels Groen <140756283+hooded-person@users.noreply.github.com>
2025-03-26 20:31:45 +0000

    Fix echoing random n's lol

--- clearData.sh
+++ clearData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -sp "Enter password: " passw && echo \n && [[ $passw == "deleteIt" ]] || exit 1;
+read -sp "Enter password: " passw && echo "" && [[ $passw == "delit" ]] || exit 1;
 source .env;
 
 echo "Choose which to delete:
@@ -10,7 +10,7 @@ echo "Choose which to delete:
 
 read selection;
 selection="${selection:-123}";
-read -p "Continue? (Y/N): " confirm && echo \n && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1;
+read -p "Continue? (Y/N): " confirm && echo "" && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1;
 
 if [[ "$selection" == *"1"* ]]; then
     rm -r "$DIRECTORY_TO_EXTRACT_TO"*;
