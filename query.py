faf3796bf869235fe810a3b47b3296a24713bec7
Niels Groen <140756283+hooded-person@users.noreply.github.com>
2025-03-27 21:44:31 +0000

    Start working on parsing ledger like queries

--- query.py
+++ query.py
@@ -75,15 +75,134 @@ def option_menu(options,name=None, offset=0):
         try:
             choice = int(choice)
         except:
-            print("Not a valid choice")
-        if choice-offset in range(len(options)):
+            print(f"{bcolors.WARNING}Not a valid choice{bcolors.ENDC}")
+            time.sleep(1)
+            continue
+        if choice - offset in range(len(options)):
             return (choice, options[choice-offset])
         print(f"{bcolors.WARNING}Not a valid choice{bcolors.ENDC}")
         time.sleep(1)
 
+# param parser
+identifiers = [
+    "Action Identifier",
+    "Object Identifier",
+    "Source Name",
+    "Time Duration",
+]
+paramsSettings = {
+    "action":{
+        "type":str,
+        "value":"Action Identifier",
+        "allowMultiple":True,
+        "allowNegative":True,
+    },"world":{
+        "type":str,
+        "value":SQLITE3_DB_TABLES,
+        "allowMultiple":True,
+        "allowNegative":True,
+    },"object":{
+        "type":str,
+        "value":"Object Identifier",
+        "allowMultiple":True,
+        "allowNegative":True,
+    },"range":{
+        "type":int,
+        "value":"int > 1",
+        "allowMultiple":False,
+        "allowNegative":False,
+    },"source":{
+        "type":str,
+        "value":"Source Name",
+        "allowMultiple":True,
+        "allowNegative":True,
+    },"before":{
+        "type":str,
+        "value":"Time Duration",
+        "allowMultiple":False,
+        "allowNegative":False,
+    },"after":{
+        "type":str,
+        "value":"Time Duration",
+        "allowMultiple":False,
+        "allowNegative":False,
+}}
+def identifierParser(identifier, value):
+
+    # if value[0] == "!" and identifier['allowNegative']:
+    #     negative = True
+    #     value = value[1:]
+    # else:
+    #     negative = False
+
+    # if type(value) != identifier['type']:
+    #     match identifier['type']:
+    #         case "int":
+    #             try: 
+    #                 value = int(value)
+    #             except ValueError:
+    #                 # success, error_message
+    #                 return False, "Could not convert to correct type"
+
+    # if type(identifier['value']) == list:
+    #     if value in identifier['value']:
+    #         if negative:
+    #             # success, [(target, value, value_prefix),...]
+    #             return True, [("table", "*"),("table", value, "NOT")]
+
+    match identifier:
+        case "Action Identifier":
+            valid = {
+                "block-break":"b",
+                "block-place":"p",
+                "block-change":None,
+                "item-insert":None,
+                "item-remove":None,
+                "entity-killed":None,
+            }
+            if value in valid:
+                return not not valid[value], valid[value] or "This Action Identifier is not supported"
+            else:
+                return False, "Not a valid Action Identifier"
+        case "Object Identifier":
+            if ":" in value:
+                value = value.split(":",maxsplit=1)[1]
+            return True, value
+        case "Source Name":
+            return True, value
+        case "Time Duration":
+            (week, day, hour, minute, second) = re.search(r'(?:(\d+)w)?(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?', value).groups()
+            print(week, day, hour, minute, second)
+            day = (day or 0) + 7*(week or 0); print(day)
+            hour = (hour or 0) + 24*(day or 0); print(hour)
+            minute = (minute or 0) + 60*(hour or 0); print(minute)
+            second = (second or 0) + 60*(minute or 0); print(second)
+            epoch = round(time.time())
+            return True, str(epoch - second)
+
+
+
+def process_params(paramsInput):
+    paramsList = paramsInput.split(" ")
+    parsedParams = {}
+    for param in paramsList:
+        key, value = param.split(":")
+        identifierParser(key, value)
+
+
+
 # Main menu options
-def inspect():
-    print("Inspection, woop woop")
+# <search|inspect> <x> <y> <z> <params>
+def query():
+    queryInput = input("Enter query:\n")
+    type, pos, paramsInput = re.search( r'(search|inspect)(?: (-?\d+)){3} (.*)',queryInput)
+    params = process_params(paramsInput)
+
+    print(pos)
+    print(flags)
+    baseQuery = f"SELECT * FROM {flags['dimension']} "
+    posQuery = f" WHERE x={pos[0]} AND y={pos[1]} AND z={pos[2]} "
+    flagQuery = handle_flags
 
 def player():
     while True:
@@ -166,11 +285,11 @@ def overview():
 
 def main():
 
-    (choice, option) = option_menu(["inspect","player","overview","exit"],name="QUERY DATABASE",offset=1)
+    (choice, option) = option_menu(["query","player","overview","exit"],name="QUERY DATABASE",offset=1)
     
     match choice:
         case 1:
-            inspect()
+            query()
         case 2:
             player()
         case 3:
@@ -179,4 +298,19 @@ def main():
             exit_prgm()
 
 if __name__ == "__main__":
-    main()
+    (choice, option) = option_menu(["main","process_params","parse identifier"], offset=1)
+    match choice:
+        case 1:
+            main()
+        case 2:
+            print(
+                process_params(
+                    input("params:\n")
+            ))
+            
+        case 3:
+            print(
+                identifierParser(
+                    input("identifier key:\n"),
+                    input("identifier value:\n")
+            ))
